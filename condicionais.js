37683f11c7835a278bbf470e6fc2f6175feff1b5
MichaelMMDev <161022176+pedro2903032@users.noreply.github.com>
2024-04-12 11:18:03 -0300

    gitjs
    gittando para vccc

---
+++ condicionais.js
@@ -0,0 +1,10 @@
+const media=8;
+
+if(media >= 7){
+ console.log('Parabens a media da turma é ${media}')
+
+}
+else{
+  console.log('Parabens voce vai repeir de ano e sua media é ${media}')
+
+}
---
+++ desafios.js
@@ -0,0 +1,10 @@
+const pontos= 90;
+
+if (pontos >100){
+
+    console.log("Parabens pelo bom trabalho,receba 1000 reais de bonificação")
+}
+else{
+
+    console.log("Voce nao atingiu a meta nescessária,trabalhe mais vagabundo")
+}
